06ac21768b85e33e7400066e90431da16acfbceb
florentbebin-gif <florent.bebin@orange.fr>
2025-11-05 12:20:41 +0100

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,80 +1,41 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import App from "./App.jsx";
-import Home from "./pages/Home.jsx";
-import Placement from "./pages/Placement.jsx";
-import Credit from "./pages/Credit.jsx";
-import Params from "./pages/Params.jsx";
-import Sim from "./pages/Sim.jsx";
-import Login from "./pages/Login.jsx";
-import ResetPassword from "./pages/ResetPassword.jsx"; // ta page reset qui fait exchangeCodeForSession
-import ProtectedRoute from "./ProtectedRoute.jsx";
-import { ParamsProvider } from "./context/ParamsProvider.jsx";
-
-import "./styles.css";
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Placement from './pages/Placement.jsx'
+import Credit from './pages/Credit.jsx'
+import Params from './pages/Params.jsx'
+import Sim from './pages/Sim.jsx'
+import Login from './pages/Login.jsx'
+import ResetPassword from './pages/ResetPassword.jsx'
+import { ParamsProvider } from './context/ParamsProvider.jsx'
+import RequireAuth from './components/RequireAuth.jsx'
+import './styles.css'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
-      // Routes protégées (affichent Login si pas de session)
-      {
-        index: true,
-        element: (
-          <ProtectedRoute>
-            <Home />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "sim/placement",
-        element: (
-          <ProtectedRoute>
-            <Placement />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "sim/credit",
-        element: (
-          <ProtectedRoute>
-            <Credit />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "params",
-        element: (
-          <ProtectedRoute>
-            <Params />
-          </ProtectedRoute>
-        ),
-      },
-      {
-        path: "sim",
-        element: (
-          <ProtectedRoute>
-            <Sim />
-          </ProtectedRoute>
-        ),
-      },
+      // PRIVÉ
+      { index: true, element: <RequireAuth><Home/></RequireAuth> },
+      { path: 'sim/placement', element: <RequireAuth><Placement/></RequireAuth> },
+      { path: 'sim/credit', element: <RequireAuth><Credit/></RequireAuth> },
+      { path: 'params', element: <RequireAuth><Params/></RequireAuth> },
+      { path: 'sim', element: <RequireAuth><Sim/></RequireAuth> },
 
-      // Routes publiques : toujours accessibles
-      { path: "login", element: <Login /> },
-      { path: "reset", element: <ResetPassword /> },
+      // PUBLIC
+      { path: 'login', element: <Login/> },
+      { path: 'reset', element: <ResetPassword/> },
     ],
   },
-  // 404 de secours
-  { path: "*", element: <div style={{ padding: 24 }}>Page introuvable</div> },
-]);
+])
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ParamsProvider>
       <RouterProvider router={router} />
     </ParamsProvider>
   </React.StrictMode>
-);
+)
