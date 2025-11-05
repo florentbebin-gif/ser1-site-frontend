6123fc318474148f9b167286fed4b7cbc6d1cbfc
florentbebin-gif <florent.bebin@orange.fr>
2025-11-05 18:11:31 +0100

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,8 +3,6 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl  = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnon = import.meta.env.VITE_SUPABASE_ANON_KEY
 
-console.log("VITE_SUPABASE_URL =", import.meta.env.VITE_SUPABASE_URL);
-console.log("VITE_SUPABASE_ANON_KEY =", import.meta.env.VITE_SUPABASE_ANON_KEY ? "OK (hidden)" : "MISSING");
 
 export const supabase = createClient(supabaseUrl, supabaseAnon, {
   auth: {
