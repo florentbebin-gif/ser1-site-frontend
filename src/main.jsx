ce190d26187f484af815b70f37efdc4e747fc5ad
florentbebin-gif <florent.bebin@orange.fr>
2025-11-14 18:08:28 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+import { waitInitialSession } from './supabaseClient.js'
 import './styles.css'
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-)
+// On attend que Supabase ait fini son travail avant de monter React
+waitInitialSession().then(() => {
+  createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  )
+})
