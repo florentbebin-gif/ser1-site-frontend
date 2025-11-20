6b5edf883b28421218a8fa763d8e1dd2a51f6c43
florentbebin-gif <florent.bebin@orange.fr>
2025-11-20 11:40:30 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home(){
         <div className="section-title strong-title">Simulateurs épargne retraite</div>
         <Tile to="/sim/potentiel" icon={<IconGauge/>} title="Epargne retraite, contrôle du potentiel" />
         <Tile to="/sim/transfert" icon={<IconTarget/>} title="PER, transfert d'un dispositif" />
-        <Tile to="/sim/strategie-is" icon={<IconHome/>} title={<><span>SERenity</span><span className="tile-note">, une approche de la capacité d'épargne</span></>} />
+        <Tile to="/sim/strategie-is" icon={<IconHome/>} title={<><span>SERenity</span><span className="tile-note">, (approche par la capacité d'épargne)</span></>} />
       </div>
 
       {/* Colonne droite */}
