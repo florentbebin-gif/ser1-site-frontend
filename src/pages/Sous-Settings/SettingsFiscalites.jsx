5b73354dff35c03c502366b57cc14428be3581c8
florentbebin-gif <florent.bebin@orange.fr>
2025-11-20 16:46:06 +0100

    Create SettingsFiscalites.jsx

---
+++ src/pages/Sous-Settings/SettingsFiscalites.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import SettingsNav from './SettingsNav';
+
+export default function SettingsFiscalites() {
+  return (
+    <div className="settings-page">
+      <div className="section-card">
+        <div className="section-title">Paramètres</div>
+        <SettingsNav />
+
+        <div style={{ marginTop: 24 }}>
+          <p>Page Fiscalités contrats (contenu à venir).</p>
+        </div>
+      </div>
+    </div>
+  );
+}
